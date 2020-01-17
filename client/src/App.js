d8c56849ea5977714085d5b41facc00f84f5e5c4
Jose <Jrivera6869@yahoo.com>
2020-01-17 11:32:39 -0500

    finished questions

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Player from "./components/Player";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    player: []
+  };
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        console.log(res.data, `data`);
+        this.setState({
+          player: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>
+          Women's World Cup players ranked by search interest from Google Trends{" "}
+        </h1>
+        <Player />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ client/src/components/PlayerGrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PlayerGrid(props) {
+    return (
+        <div>
+            <p>PlayerGrid</p>
+        </div>
+    )
+}
